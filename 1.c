d2b7e43eee39b9596287680ae68bcdaaa3dcc464
Senseyka1 <74878665+Senseyka1@users.noreply.github.com>
2021-04-15 18:10:43 +0300

    Update 1.c

--- 1.c
+++ 1.c
@@ -3,19 +3,20 @@
 
 
 int main () {
-	int a, b, kor, dob, dif;
+	int a, b, dob, dif;
+    	float kor;
 
-        printf("Введіть число a: ");
-        scanf("%d",&a );
-        printf("Введіть число b: ");
-        scanf("%d",&b );
+    	printf("Введіть число a: ");
+    	scanf("%d",&a );
+    	printf("Введіть число b: ");
+    	scanf("%d",&b );
 
-	kor = sqrt (a);
+	kor = pow(a, 0.33);
 	dob = 2  *  (a + b);
 	dif = a  -  (3 * b);
   
-        printf("\n***Результат***\n");
-	printf("1 = %d\n2 = %d\n3 = %d\n", kor, dob, dif);
+   	printf("\n***Результат***\n");
+	printf("1 = %.0f\n2 = %d\n3 = %d\n", kor, dob, dif);
 
 	return 0;
   }
